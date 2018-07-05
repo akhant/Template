de765c26615a5a0faf0346fa63a2845fb72e74fa
akhant <a.khantelev@mail.ru>
2018-07-05 15:13:24 +0300

    do it

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Main extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+  state ={
+
+  }
+
+  render() {
+    return <div>Hello</div>;
+  }
+}
